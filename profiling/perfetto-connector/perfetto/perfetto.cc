bef3a4e95fb6d4bd1453528a62b18e294a364269
Vivek Kale <vivek.lkale@gmail.com>
2024-07-11 12:23:02 -0700

    perfetto.cc: remove stray garbled text due vi editor problems

--- profiling/perfetto-connector/perfetto/perfetto.cc
+++ profiling/perfetto-connector/perfetto/perfetto.cc
@@ -2472,7 +2472,7 @@ void InstallCtrCHandler(CtrlCHandlerFunction);
 /*
  * Copyright (C) 2021 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");InlineChunkHeaderSize'
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
